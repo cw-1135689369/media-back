36d5257977f14f9facc461dbbe5831481b9a2fe3
cw-1135689369 <1135689369@qq.com>
2020-04-30 16:20:59 +0800

    传统媒体分析后端代码

---
+++ media-back-master/Flask_Project/service/ReporterAnalysis.py
@@ -0,0 +1,305 @@
+import sys
+sys.path.append(r"./dao")
+from datetime import datetime
+import json
+import pandas as pd
+from collections import Counter
+import datetime
+
+# 根据date获取 当天日期和date天之前日期的列表
+def getDateList(date):
+    before_n_days = []
+    before_n_days.append(str(datetime.date.today()))
+    before_n_days.append((str(datetime.date.today() - datetime.timedelta(days=date))))
+    return before_n_days
+
+# 获取全部数据的方法
+def getDataByDate(date):
+    # 获取数据
+    df = pd.read_csv("./static/datas/reporter_analysis.csv")
+    # 新增时间类型的列
+    df.loc[:, "date_date"] = pd.to_datetime(df['str_date'], format='%Y%m%d')
+    # 获取date天前的时间列表
+    date_list = getDateList(date)
+    count_index = pd.date_range(start=date_list[1], end=date_list[0])
+    # 选取时间列表相等的数据
+    data = df.loc[df['date_date'].isin(count_index)]
+    data = data[~df['reporter'].isin(['-1'])]
+    data = data[~df['reporter'].isin([''])]
+    return data
+
+
+# 将dataframe 转化成 json
+def to_json(df,orient='split'):
+    df_json = df.to_json(orient = "records", force_ascii = False)
+    return json.loads(df_json)
+# 分页获取首页发文量前五十的记者相关信息
+def getData(date):
+    # 获取数据
+    df = getDataByDate(date)
+    # 删除没有记者的数据
+    start = time.clock()
+    df = df[~df['reporter'].isin([''])]
+    df = df[~df['reporter'].isin(['-1'])]
+    # 合并媒体，记者  （不同媒体可能存在同名记者）
+    df["media_reporter"] = df["media"] + "|" + df["reporter"]
+    # 获得发文量前五十的记者，媒体，发文数。
+    reporterData = df["media_reporter"].value_counts()
+    reporter_data = list(reporterData.items())
+    # 列表处理
+    data = []
+    for reporter in reporter_data:
+        a = []
+        a = reporter[0].split("|")
+        a.append(reporter[1])
+        data.append(a)
+    return data
+
+# 记者详情页页面数据获取
+# 记者关注的基金公司
+def getReporterConcernCompany(date,media,reporter):
+    # 获取数据
+    df = getDataByDate(date)
+    # 选取媒体  符合条件的记者
+    copy = df.query("media == %s" % (media))
+    copy = copy.query("reporter == %s" % (reporter))
+    # 对记者关注的基金公司进行分析
+    str = ''
+    company_data = copy["key1"].values.tolist()
+    # 对字符串进行处理,得到列表中所有字符串
+    for company in company_data:
+        str = str + company
+    # 去除字符串的开头的空白
+    str = str[1:len(str)]
+    # 将字符串切割插入列表
+    data = str.split(" ")
+    # 统计基金公司出现次数
+    result = Counter(data)
+    return result
+
+# 获取记者负面关注的基金公司
+def getReporterHostilityCompany(date,media,reporter):
+    # 获取数据
+    df = getDataByDate(date)
+    # 选取媒体  符合条件的记者
+    copy = df.query("media == %s" % (media))
+    copy = copy.query("reporter == %s" % (reporter))
+    # 对记者关注的基金公司进行分析
+    str = ''
+    company_data = copy.query(("scores ==-2"))["key1"].values.tolist()
+    if len(company_data) == 0:
+        return "无负面新闻"
+    # 对字符串进行处理,得到列表中所有字符串
+    for company in company_data:
+        str = str + company
+    # 去除字符串的开头的空白
+    str = str[1:len(str)]
+    # 将字符串切割插入列表
+    data = str.split(" ")
+    # 统计基金公司出现次数
+    result = Counter(data)
+    return result
+
+# 获取记者发表的负面文章
+def getMediaNegativeNewsListByreporter(reporter, date):
+    # 获取全部数据
+    df = getDataByDate(date)
+    # 得到相关媒体的数据
+    copy = df.query("reporter == %s" % (reporter))
+    # 获取负面新闻呢
+    news_data_negative = copy.query("scores == -2")
+    print(news_data_negative)
+    data = news_data_negative.loc[:, ['post_id', 'media', 'title', 'link', 'grp_copy', 'str_date']]
+    return to_json(data)
+# 获取记者正面新闻
+def getMediapositiveNewsListByreporter(reporter , date):
+    # 获取全部数据
+    df = getDataByDate(date)
+    # 得到相关媒体的数据
+    copy = df.query("reporter == %s" % (reporter))
+    # 获取负面新闻呢
+    news_data_negative = copy.query("scores == 2")
+    data = news_data_negative.loc[:, ['post_id', 'media', 'title', 'link', 'grp_copy', 'str_date']]
+    return to_json(data)
+# 获取记者关注话题
+def getConversationtListByreporter(reporter, date):
+    # 获取全部数据
+    df = getDataByDate(date)
+    copy = df.query("reporter == %s" % (reporter))
+    data = copy['category']
+    list = data.values.tolist()
+    b = str(list)
+    b = b.replace('[', '').replace(']', '').replace('\'', '').replace(',', '').replace(' ', '').replace('nan', '')
+    d = []
+    c = {"H": "经理发表的投研观点", "@": "主动曝光", "Y": "公司运作", "J": "产品绩效", "T": "产品投资", "S": "产品规模", "A": "产品运作", "N": "社保年金专户",
+         "B": "公司业绩", "U": "公司总体规模", "V": "公司股东", "F": "评价", "Z": "高管", "D": "高管变动", "G": "ESG", "M": "离职人员",
+         "O": "子公司", "E": "入选组合", "P": "经理评价", "R": "经理变动", "\#": "敏感信息", "&": "低权威", "*": "业绩盘点", "$": "推荐",
+         "W": "营销活动", "K": "客户"}
+    for a in b:
+        d.append(c.get(a))
+    word_counts = Counter(d)
+    dict = word_counts.items()
+    l = []
+    for key, value in dict:
+        d = {}
+        d["name"] = key
+        d["value"] = value
+        l.append(d)
+    return l
+
+
+# 获取记者发文性格
+    import time
+def getCharacter(reporter, media):
+    # 获取180天的数据
+    df = getDataByDate(180)
+    # 选取记者数据
+    copy = df.query("reporter == %s" % (reporter))
+    # 记者日均发文量
+    reporter_average_count = len(copy) / 180
+    # 记者评分
+    num_score = 0
+    if reporter_average_count>=1 :
+        num_score =100
+    else:
+        reporter_average_count < 1
+        num_score =int(reporter_average_count * 100)
+    # 记者影响力分析
+    # 获取总转载数
+    Total_reprint = copy['grp_copy'].sum()
+    # 平均转载数
+    average_reprint = Total_reprint / len(copy)
+    # 最高转载数
+    heigh_reprint = copy['grp_copy'].max()
+    # 获取影响力 计算方法：传播效果=0.6*文章总转载数+0.2*最高转载数+0.2*篇均转载数
+    effecte = 0.6 * Total_reprint + 0.2 * heigh_reprint + 0.2 * average_reprint
+    effecte = int (effecte)
+    # 评分逻辑
+    effecte_scores = 0
+    if effecte >= 100:
+        effecte_scores = 100
+    else:
+        effecte_scores = effecte
+
+    # 正面关注的文章数量
+    positive_count = len(copy.query("scores == 2")) + len(copy.query("scores == 1")) + len(copy.query("scores == 3"))
+    # 负面面关注的文章数量
+    negative_count = len(copy.query("scores == -2")) + len(copy.query("scores == -1")) + len(copy.query("scores == -3"))
+    # 中性文章
+    positive = positive_count / len(copy)
+    negative = negative_count / len(copy)
+    positive_scores = 0
+    negative_scores = 0
+    # 正面评分规则
+    if positive >= 0.2:
+        positive_scores = 100
+    elif 0 < positive < 0.2:
+        positive_scores = (positive * 200) + 60
+    # 负面评分规则
+    if negative >= 0.2:
+        negative_scores = 100
+    elif 0 < negative < 0.2:
+        negative_scores = (negative * 200) + 60
+
+    # 将str_date 转换成 dataframe日期类型
+    copy = df.query("reporter == %s" % (reporter))
+    copy['str_date'] = copy['str_date'].apply(str)
+    copy.loc['str_date'] = copy['str_date'][0:8]
+    copy['str_date'] = pd.to_datetime(copy['str_date'], format='%Y%m%d')
+    # 统计每天发文量
+    count_data = copy['str_date'].value_counts()
+    # 1.生成完整时间序列
+    dateList = getDateList(180)
+    count_index = pd.date_range(start=dateList[1], end=dateList[0])
+    # 2.填充数据
+    count_data_new = count_data.reindex(count_index, fill_value=0)
+
+    # 如果总数小于50
+    stable_score = 0
+    if Total_reprint <= 10:
+        stable_score = 50
+    else:
+        # 如果日均发文量大于1
+        if reporter_average_count >= 1:
+            # 求发文量标准差
+            media_count_std = count_data_new.std()
+            # 发文量离散系数
+            dispersion_coefficient = media_count_std / reporter_average_count
+            # 稳定性打分
+            if dispersion_coefficient <= 1.3:
+                stable_score = 100
+            elif 1.3 < dispersion_coefficient <= 1.4:
+                stable_score = 90
+            elif 1.4 < dispersion_coefficient <= 1.5:
+                stable_score = 80
+            elif 1.5 < dispersion_coefficient <= 1.6:
+                stable_score = 70
+            elif 1.6 < dispersion_coefficient <= 1.7:
+                stable_score = 60
+            elif 1.7 < dispersion_coefficient <= 1.8:
+                stable_score = 50
+            elif 1.8 < dispersion_coefficient:
+                stable_score = 40
+        else:
+            if count_data_new.max() - count_data_new.min() <= 3:
+                stable_score = 100
+            elif count_data_new.max() - count_data_new.min() == 4:
+                stable_score = 90
+            elif count_data_new.max() - count_data_new.min() == 5 or count_data_new.max() - count_data_new.min() == 6:
+                stable_score = 80
+            elif count_data_new.max() - count_data_new.min() == 7 or count_data_new.max() - count_data_new.min() == 8:
+                stable_score = 70
+            elif count_data_new.max() - count_data_new.min() == 9 or count_data_new.max() - count_data_new.min() == 10:
+                stable_score = 60
+            elif count_data_new.max() - count_data_new.min() > 10:
+                stable_score = 50
+    dict = {
+        "num_score":str(num_score),
+        "effecte_scores":str(effecte_scores),
+        "positive_scores":str(positive_scores),
+        "negative_scores":str(negative_scores),
+        "stable_score":str(stable_score)
+    }
+    return dict
+
+# 根据date获取 当天日期和date天之前日期的列表
+def getDateList(date):
+    before_n_days = []
+    before_n_days.append(str(datetime.date.today()))
+    before_n_days.append((str(datetime.date.today() - datetime.timedelta(days=date))))
+    return before_n_days
+
+
+
+
+
+
+
+
+
+
+
+def a ():
+    # 获取180天的数据
+    df = getDataByDate(180)
+    # 获取记者列表
+    rep = pd.read_csv("./static/datas/reporter_list.csv")
+    data = rep.iloc[:,0]
+    data = data.values.tolist()
+    a = []
+    for reporter in data:
+        copy = df[df['reporter'].isin([reporter])]
+        # print(copy['reporter'])
+        p = len(copy.query("scores == 2")) + len(copy.query("scores == 1")) + len(copy.query("scores == 3"))
+        n = len(copy.query("scores == -2")) + len(copy.query("scores == -1")) + len(copy.query("scores == -3"))
+        z = len(copy.query("scores == 0"))
+        b = [reporter,p,n,z,len(copy)]
+        a.append(b)
+    c = pd.DataFrame(a)
+    c.to_csv(path_or_buf="./static/datas/reporter_list.csv", sep=',', index=False, header=True)
+    print(c)
+    return "qqq"
+
+
+
+
---
+++ media-back-master/Flask_Project/zlktqa.py
@@ -0,0 +1,283 @@
+#encoding:utf-8
+from flask import Flask,render_template,request,redirect,url_for,session
+import pymysql
+import config
+import sys
+sys.path.append(r"./service")
+import BigMediaAnalysis
+import ReporterAnalysis
+import MediaAnalysis , Setter
+
+sys.path.append(r"./dao")
+import BigMediaAnalysisDao
+import MediaAnalysisDao
+import ReporterAnalysisDao
+
+import DoMysql
+from flask import jsonify
+import pandas as pd
+import numpy as np
+import json
+import datetime
+from flask_cors import *
+from flask_script import  Manager
+import csv
+
+app=Flask(__name__)
+app.config.from_object(config)
+CORS(app ,supports_credentials = True)
+app.config['JSON_AS_ASCII'] = False
+
+
+'''
+    日志管理
+'''
+
+'''
+    设置类接口
+'''
+# 设置查询时间
+@app.route("/setDateById")
+def setDateById():
+    id = request.args.get("id", type=int)
+    date = request.args.get("date", type=int)
+    u = Setter.setDateById(date,id)
+    return jsonify(u)
+# 根据id查询设定的时间
+@app.route("/getDateById")
+def getDateById():
+    id = request.args.get("id", type=int)
+    u = Setter.getDateById(id)
+    return jsonify(u)
+
+'''
+    媒体大数据页面
+'''
+# 查询列表需要数据
+@app.route("/getBigMediaFormByDate")
+def getBigMediaFormByDate():
+    date = request.args.get("date", type=int)
+    u = BigMediaAnalysis.getBigMediaFormByDate(date)
+    return jsonify(u)
+
+# 查询媒体发文量前十的数据（直方图数据）
+@app.route("/getBigMediaChartByDate")
+def getBigMediaChartByDate():
+    date=request.args.get("date", type=int)
+    u=BigMediaAnalysis.getBigMediaChartByDate(date)
+    return jsonify(u)
+
+# 获取折线图数据
+@app.route("/getBigMediaBrokenlineChartByDate")
+def getBigMediaBrokenlineChartByDate():
+    date=request.args.get("date", type=int)
+    u=BigMediaAnalysis.getBigMediaBrokenlineChartByDate(date)
+    return jsonify(u)
+
+# 媒体大数据页解释数据
+@cross_origin()
+@app.route("/getBigMediaAnalysis")
+def getBigMediaAnalyysis():
+    date = request.args.get("date", type=int)
+    u = BigMediaAnalysis.getDataForBigMediaAnalysis(date)
+    return u
+
+'''
+    媒体解释页面
+'''
+# 根据媒体获取该媒体发文量前三的记者   参数  媒体 时间
+@app.route('/getReporterBymediaAndDate')
+def getReporterBymedia():
+    media= request.args.get("media")
+    date=request.args.get("date",type=int)
+    u = MediaAnalysis.getReporter(media,date)
+    return jsonify(u)
+
+# （折线图数据）
+@app.route("/getBigMedia")
+def getBigMedia():
+    date = request.args.get("date", type=int)
+    media = request.args.get("media")
+    u=MediaAnalysis.getNewsDaylyCountForChartBymedia(date,media)
+    return jsonify(u)
+
+# 获取媒体对不同基金公司的敌意占比（饼状图数据）
+@app.route("/getHostilityRatio")
+def getHostilityRatio():
+    date = request.args.get("date", type=int)
+    media = request.args.get("media")
+    u = MediaAnalysis.getHostilityRatio(date, media)
+    return jsonify(u)
+
+# 媒体分析页解释所需要的数据
+@app.route('/getMediaDate')
+def getediaMDate():
+    media= request.args.get("media")
+    date=request.args.get("date", type=int)
+    u = MediaAnalysis.getDataForMediaAnalysis(media, date)
+    return jsonify(u)
+# 获取媒体正面负面关注的新闻
+@app.route('/getMediaPositiveNewsListByMedia')
+def getMediaPositiveNewsListByMedia():
+    media= request.args.get("media")
+    date=request.args.get("date", type=int)
+    u = MediaAnalysis.getMediaPositiveNewsListByMedia(media, date)
+    return jsonify(u)
+# 获取媒体负面关注的新闻列表
+@app.route('/getMediaNegativeNewsListByMedia')
+def getMediaNegativeNewsListByMedia():
+    media= request.args.get("media")
+    date=request.args.get("date", type=int)
+    u = MediaAnalysis.getMediaNegativeNewsList(media, date)
+    return jsonify(u)
+# 根据post_id获取分组文章
+@app.route('/getAllNewsByPostId')
+def getAllNewsByPostId():
+    post_id=request.args.get("post_id", type=int)
+    u = MediaAnalysis.getAllNewsByPostId(post_id)
+    return jsonify(u)
+
+
+'''
+    记者页面相关接口
+'''
+# 根据记者发文量统计首页显示数据
+@app.route('/getReporterBydate')
+def getReporterForDate():
+    date=request.args.get("date", type=int)
+    u =ReporterAnalysis.getData(date)
+    return jsonify(u)
+# 根据媒体  记者姓名 获取记者关注的基金公司
+@app.route('/getReporterConcernCompany')
+def getReporterConcernCompany():
+    media = request.args.get("media")
+    reporter = request.args.get("reporter")
+    date = request.args.get("date", type=int)
+    u =ReporterAnalysis.getReporterConcernCompany(date,media,reporter)
+    return jsonify(u)
+# 根据媒体  记者姓名 获取记者敌意基金公司
+@app.route('/getReporterHostilityCompany')
+def getReporterHostilityCompany():
+    media = request.args.get("media")
+    reporter = request.args.get("reporter")
+    date=request.args.get("date", type=int)
+    u =ReporterAnalysis.getReporterHostilityCompany(date,media,reporter)
+    return jsonify(u)
+# 根据记者名获取该记者的负面关注新闻
+@app.route('/getMediaNegativeNewsListByreporter')
+def getMediaNegativeNewsListByreporter():
+    reporter= request.args.get("reporter")
+    date=request.args.get("date", type=int)
+    u = ReporterAnalysis.getMediaNegativeNewsListByreporter(reporter, date)
+    return jsonify(u)
+# 获取记者正面关注新闻
+@app.route('/getMediapositiveNewsListByreporter')
+def getMediapositiveNewsListByreporter():
+    reporter= request.args.get("reporter")
+    date=request.args.get("date", type=int)
+    u = ReporterAnalysis.getMediapositiveNewsListByreporter(reporter, date)
+    return jsonify(u)
+# 根据媒体记者名获取记者关注话题
+@app.route('/getConversationtListByreporter')
+def getConversationtListByreporter():
+    reporter= request.args.get("reporter")
+    date=request.args.get("date", type=int)
+    u = ReporterAnalysis.getConversationtListByreporter(reporter, date)
+    return jsonify(u)
+# todo 雷达图需要数据
+'''
+    每个刻度最多一百分，对每名记者进行打分设置，五个维度发文量 影响力 正面倾向 负面倾向 发文离散程度 
+    测试中
+'''
+@app.route('/getCharacter')
+def getCharacter():
+    reporter = request.args.get("reporter")
+    media = request.args.get("media", type=int)
+    u = ReporterAnalysis.getCharacter(reporter, media)
+    return jsonify(u)
+
+
+
+
+
+
+
+
+
+# todo 雷达图需要数据
+@app.route('/a')
+def a():
+    u = ReporterAnalysis.a()
+    return jsonify(u)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# todo 定时任务
+from flask_apscheduler import APScheduler
+
+#任务配置类
+class SchedulerConfig(object):
+    JOBS = [
+        {
+            'id': 'get_data_job', # 任务id
+            'func': '__main__:get_data_job', # 任务执行程序
+            'args': None, # 执行程序参数
+            'trigger': 'cron',  # cron表示定时任务
+            'hour': 17,
+            'minute': 18
+        }
+    ]
+
+# 定义任务执行程序
+def get_data_job():
+    # 读取数据库数据(媒体大数据页面)
+    u=BigMediaAnalysisDao.getBigMediaByDateAll()
+    # 将数据转化成dataframe
+    df_big_media_analysis = pd.DataFrame(u, columns=['media', 'str_date', 'reprint', 'scores'])
+    path = "./static/datas/big_media_analysis.csv"
+    df_big_media_analysis.to_csv(path_or_buf = path , sep=',', index=False, header=True)
+
+    # 读取数据库数据(媒体分析页面)
+    v = MediaAnalysisDao.getMediaAnalysisDataByDateall()
+    # 将数据转化成dataframe
+    df_media_analysis = pd.DataFrame(v, columns=['post_id','media', 'reporter','str_date','title','link','scores','grp_copy','key1'])
+    path_meida = "./static/datas/media_analysis.csv"
+    df_media_analysis.to_csv(path_or_buf=path_meida, sep=',', index=False, header=True)
+
+    # 记者画像页面
+    s = ReporterAnalysisDao.getReporterAnalysisDataByDateAll()
+    # 将数据转化成dataframe
+    df_media_analysis = pd.DataFrame(s, columns=['post_id','media', 'reporter','str_date','title','link','scores','grp_copy','key1','category'])
+    path_meida = "./static/datas/reporter_analysis.csv"
+    df_media_analysis.to_csv(path_or_buf=path_meida, sep=',', index=False, header=True)
+#为实例化的flask引入定时任务配置
+app.config.from_object(SchedulerConfig())
+
+
+
+
+
+@app.route('/job')
+def job():
+    get_data_job()
+    return '1'
+
+
+if __name__=='__main__':
+    scheduler = APScheduler()  # 实例化APScheduler
+    scheduler.init_app(app)  # 把任务列表载入实例flask
+    scheduler.start()  # 启动任务计划
+    app.run()
+
+
